50406085983d7896e1a3f0130b06c08d82cc95b0
Test3413 <58014467+Test3413@users.noreply.github.com>
2019-11-20 20:07:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  
+  <form action="index.php" method="POST">
+    <input type="text" name="stars1">
+    <input type="text" name="stars2">
+    <input type="text" name="stars3">
+    <input type="text" name="stars4">
+    <input type="text" name="stars5">
+    <input type="submit" value="Randomize">
+  </form>
+</html>
